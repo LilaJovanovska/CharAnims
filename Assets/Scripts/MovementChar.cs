44ccd455ac2c5e9ab1d2b98655629769ddd481b5
KristijanJovanovskii <157608234+LilaJovanovska@users.noreply.github.com>
2024-07-26 16:23:38 +0200

    update

--- Assets/Scripts/MovementChar.cs
+++ Assets/Scripts/MovementChar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovementChar : MonoBehaviour
@@ -11,10 +12,14 @@ public class MovementChar : MonoBehaviour
 
     [SerializeField] private GameObject ground;
 
+    private Animator _animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
@@ -24,28 +29,44 @@ public class MovementChar : MonoBehaviour
         SimpleDodge();
         DodgeSideways();
         Jump();
+        Run();
+    }
+
+    private void Run()
+    {
+        var isRunning = Input.GetKey(KeyCode.LeftShift);
+        var isRunningHash = Animator.StringToHash("isRunning");
+        _animator.SetBool(isRunningHash, isRunning);
+        var isRunningInjured = Input.GetKey(KeyCode.LeftControl);
+        var isRunningInjuredHash = Animator.StringToHash("isHpLow");
+        _animator.SetBool(isRunningInjuredHash, isRunningInjured);
     }
 
     private void MoveLeftAndRight()
     {
-        if (Input.GetKeyDown(KeyCode.A)) 
-        {
-            
-        }
+        var isWalking = Input.GetKey(KeyCode.A)
+                        || Input.GetKey(KeyCode.D)
+                        || Input.GetKey(KeyCode.W)
+                        || Input.GetKey(KeyCode.S);
+        var isWalkingHash = Animator.StringToHash("isWalking");
+        _animator.SetBool(isWalkingHash, isWalking);
     }
 
     private void SimpleDodge()
     {
-
+        var isDodging = Input.GetKey(KeyCode.Space);
+        var isDodgingHash = Animator.StringToHash("isDodging");
+        _animator.SetBool(isDodgingHash, isDodging);
     }
 
     private void DodgeSideways()
     {
-
     }
 
     private void Jump()
     {
-
+        var isJumping = Input.GetKey(KeyCode.F);
+        var isJumpingHash = Animator.StringToHash("isJumping");
+        _animator.SetBool(isJumpingHash, isJumping);
     }
 }
